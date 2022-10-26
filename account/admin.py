9f37aeed7f8c7f485be9f1464d411786068642bb
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2022-10-26 10:04:45 +0600

    Custom User Model Admin

--- account/admin.py
+++ account/admin.py
@@ -1,1 +1,24 @@
 from django.contrib import admin
+from account.models import User
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+ 
+class UserModelAdmin(BaseUserAdmin):
+    list_display = ('id','email', 'name', 'tc', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ('User', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'tc')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name','tc', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email','name','id',)
+    ordering = ('email','id')
+    filter_horizontal = ()
+
+admin.site.register(User, UserModelAdmin)
