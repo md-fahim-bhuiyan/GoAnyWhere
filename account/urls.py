6fb3f8b9429d6bfc7ceff21fa77bc32a78bcf3ad
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2022-10-27 17:04:43 +0600

    Password Change

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from account.views import UserProfileView, UserRegistrationView, UserLoginView
+from account.views import UserChangePasswordView, UserProfileView, UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     # path('signup/', views.signup, name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-
+    path('passwordchange/', UserChangePasswordView.as_view(), name='passwordchange'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 
+from multiprocessing import context
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationserializer ,UserLoginSerializer, UserProfileSerializer
+from account.serializers import UserRegistrationserializer ,UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
 from django.contrib.auth import authenticate  
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -48,3 +49,11 @@ class UserProfileView(APIView):
     def get(self, request, format=None):
         serializer = UserProfileSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserChangePasswordView(APIView):
+  renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def post(self, request, format=None):
+    serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
