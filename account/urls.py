ea2f6a9961a59a252665752e6c441b6fc91c85ae
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2022-11-11 16:48:37 +0600

    new

--- account/urls.py
+++ account/urls.py
@@ -2,17 +2,6 @@ from django.urls import path
 from account.views import UserRegistrationView
 from . import views
 urlpatterns = [
-    # path('register', views.register, name='register'),
-    
-    # # path('signup/', views.signup, name='signup'),
-    # path('login', views.login, name='login'),
-    # path('login/view/', UserLoginView.as_view(), name='loginview'),
-    # path('profile/', UserProfileView.as_view(), name='profile'),
-    # path('newpassword/passwordchange/', UserChangePasswordView.as_view(), name='passwordchange'),
-    # path('sendresetpasswordemail/', SendPasswordResetEmailView.as_view(), name='sendresetpasswordemail'),
-    # path('logout/', UserLogoutView.as_view(), name='logout'),
-    # path('profileview/', views.profileview, name='profileview'),
-    # path('newpassword/', views.newpassword, name='newpassword'),
     path('', views.home, name='home'),    
     path('register/', views.register, name='register'),
     path('register/done', UserRegistrationView.as_view(), name='registerdone'),
